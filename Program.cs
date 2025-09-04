162c5c054f9afa1ad4d03a0cecdc992146d1a72a
339729139 <339729139@gotvdsb.ca>
2025-09-04 09:21:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Topic___The_Basics_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Sam";
+            string favMovie = "The Matrix";
+            Console.WriteLine($"Hello, {firstName.ToLower}");
+        }
+    }
+}
