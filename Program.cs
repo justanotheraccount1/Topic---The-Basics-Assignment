4ecc73327f029d5eedc4a80bd8d6bcfda812c267
339729139 <339729139@gotvdsb.ca>
2025-09-05 09:40:08 -0400

    almost done

--- Program.cs
+++ Program.cs
@@ -8,6 +8,82 @@
             string favMovie = "The Matrix";
             Console.WriteLine($"hello, {firstName.ToLower()}. the best movie is {favMovie.ToLower()}");
             favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            string movieQuote = "May the force be with you";
+            movieQuote = movieQuote.ToLower();
+            movieQuote = movieQuote.Replace("a", " ");
+            movieQuote = movieQuote.Replace("e", " ");
+            movieQuote = movieQuote.Replace("i", " ");
+            movieQuote = movieQuote.Replace("o", " ");
+            movieQuote = movieQuote.Replace("u", " ");
+            movieQuote = movieQuote.Replace("y", " ");
+            Console.WriteLine(movieQuote);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+            Console.Write("          c,_.--.,y");
+            Console.WriteLine("                     __");
+            Console.Write("            7 a.a(");
+            Console.WriteLine("          .-.__      \\ .-.  ___  __");
+            Console.Write("           (   ,_Y)");
+            Console.WriteLine("          |_|  '--.-.-(   \\/\\;;\\_\\.-._______.-.");
+            Console.Write("           :  '---;");
+            Console.WriteLine("          (-)___     \\ \\ .-\\ \\;;\\(   \\       \\ \\");
+            Console.Write("       ___.'\\.  - (");
+            Console.WriteLine("           Y    '---._\\_((Q)) \\;;\\\\ .-\\     __(_)\r\n");
+            Console.Write("     .'\"\"\"S,._'--'_2..,_");
+            Console.WriteLine("           I           __'-' / .--.((Q))---'    \\,");
+            Console.Write("     |    ':::::=:::::  \\");
+            Console.WriteLine("           I     ___.-:    \\|  |   \\'-'_          \\");
+            Console.Write("     .     f== ;-,---.' T");
+            Console.WriteLine("           A  .-'      \\ .-.\\   \\   \\ \\ '--.__     '\\");
+            Console.Write("      Y.   r,-,_/_      |");
+            Console.WriteLine("           |  |____.----((Q))\\   \\__|--\\_      \\     '");
+            Console.Write("      |:\\___.---' '---./");
+            Console.WriteLine("              ( )        '-'  \\_  :  \\-' '--.___\\");
+            Console.Write("      |'`             )");
+            Console.WriteLine("               Y                \\  \\  \\       \\(_)");
+            Console.Write("       \\             ,");
+            Console.WriteLine("               I                 \\  \\  \\         \\,");
+            Console.Write("       ':;,.________.;L");
+            Console.WriteLine("               I                  \\  \\  \\          \\");
+            Console.Write("       /  '---------' |");
+            Console.WriteLine("               A                   \\  \\  \\          '\\\r\n");
+            Console.Write("       |              \\");
+            Console.WriteLine("               |                    \\  \\__|           '");
+            Console.Write("       |              \\");
+            Console.WriteLine("                                     \\_:.  \\\r\n");
+            Console.Write("       L---'-,--.-'--,-'");
+            Console.WriteLine("                                       \\ \\  \\\r\n");
+            Console.Write("        T    /   \\   Y");
+            Console.WriteLine("                                        \\ \\  \\\r\n");
+            Console.Write("        |   Y    ,   |");
+            Console.WriteLine("                                         \\_\\_|\r\n");
+            Console.WriteLine("        (   )     \\,_L");
+            Console.WriteLine("        7-./      )  `,");
+            Console.WriteLine("       /  _(      '._  \\");
+            Console.WriteLine("     '---'           '--'");
+
+
         }
     }
 }
